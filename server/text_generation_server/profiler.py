6cf40ab05a9019570010140fd0b5b73f2423e639
Krzysztof Laskowski <klaskowski@habana.ai>
2024-01-12 18:23:56 +0200

    Make profiler dump events to file asynchronously

--- server/text_generation_server/profiler.py
+++ server/text_generation_server/profiler.py
@@ -1,28 +1,60 @@
-import asyncio
 import os
-import sys
-import torch
+import threading
+import queue
 
 from contextlib import contextmanager
 import time
 import json
 
+class FileWriter(threading.Thread):
+    def __init__(self, filename, write_queue):
+        super().__init__()
+        self.filename = filename
+        self.write_queue = write_queue
+        self.daemon = True
+        self.timer_event = threading.Event()
+
+    def _drain_write_queue(self):
+        content = ""
+        while True:
+            try:
+                element = self.write_queue.get_nowait()
+                content += element
+            except queue.Empty:
+                break
+        return content
+
+    def run(self):
+        # don't check the queue too often
+        while not self.timer_event.wait(1):
+            # Block and wait for the next item in the queue
+            content = self.write_queue.get()
+            # Collect any other items in the queue
+            content += self._drain_write_queue()
+
+            with open(self.filename, "a") as outfile:
+                outfile.write(content)
+
 class Profiler():
-    profiling_trace_events = []
-    event_tid = {"counter": 1, "external": 2, "internal": 3}
+    profiling_trace_events = queue.Queue()
+    event_tid = {"counter": 1, "external": 2, "internal": 3, "own": 4}
+    filename = "server_events.json"
 
     def __init__(self):
         self.enabled = os.getenv("TGI_PROFILER_ENABLED", "false").lower() == "true" and int(os.getenv("RANK", "0")) == 0
         if self.enabled:
-            with open("server_events.json", "w") as outfile:
+            # initialize the trace file
+            with open(self.filename, "w") as outfile:
                 outfile.write('{"traceEvents": ')
+            file_writer = FileWriter(self.filename, self.profiling_trace_events)
+            file_writer.start()
 
     @contextmanager
     def record_event(self, type, name, args=None, util=None):
         if self.enabled:
             start = time.time() * 1000000.0
             if util is not None:
-                self.profiling_trace_events.append({
+                self.profiling_trace_events.put(json.dumps([{
                 "pid": 1,
                 "tid": self.event_tid["counter"],
                 "ph": "C",
@@ -30,8 +62,7 @@ class Profiler():
                 "ts": start,
                 "args": {
                     "util": util["util"],
-                    #"queue": util["queue_len"]
-                }})
+                }}]))
 
             event = {
                 "pid": 1,
@@ -46,11 +77,7 @@ class Profiler():
 
             end = time.time() * 1000000.0
             event["dur"] = end - start
-            self.profiling_trace_events.append(event)
 
-            if len(self.profiling_trace_events) > 100:
-                with open("server_events.json", "a") as outfile:
-                    outfile.write(json.dumps(self.profiling_trace_events))
-                self.profiling_trace_events = []
+            self.profiling_trace_events.put(json.dumps([event]))
         else:
             yield
